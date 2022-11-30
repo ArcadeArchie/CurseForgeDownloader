8222809c85680a3c201288d40644d33ba899d43b
ArcadeArchie <29282748+ArcadeArchie@users.noreply.github.com>
2022-11-30 05:19:43 +0100

    Added darkmode switch with config setting `EnableDarkMode`

--- CurseForgeDownloader/App.axaml.cs
+++ CurseForgeDownloader/App.axaml.cs
@@ -2,16 +2,26 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Avalonia.Themes.Fluent;
 using CurseForgeDownloader.ViewModels;
 using CurseForgeDownloader.Views;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CurseForgeDownloader
 {
     public partial class App : Application
     {
+        private readonly IConfiguration _config;
+
+        public App()
+        {
+            _config = Program.AppHost!.Services.GetRequiredService<IConfiguration>();
+        }
         public override void Initialize()
         {
+            var useDarkMode = _config.GetValue<bool>("EnableDarkMode");
+            DataContext = useDarkMode ? FluentThemeMode.Dark : FluentThemeMode.Light;
             AvaloniaXamlLoader.Load(this);
         }
 
@@ -19,7 +29,7 @@ namespace CurseForgeDownloader
         {
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
-                    desktop.MainWindow = new MainWindow();
+                desktop.MainWindow = new MainWindow();
             }
 
             base.OnFrameworkInitializationCompleted();
