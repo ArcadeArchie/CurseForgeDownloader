8711b3c46ea74a9c3ef7cf88cf3daabe03c88cf3
ArcadeArchie <29282748+ArcadeArchie@users.noreply.github.com>
2022-11-30 04:32:09 +0100

    Reduced Boilerplate code for VMBase properties

--- CurseForgeDownloader/ViewModels/ViewModelBase.cs
+++ CurseForgeDownloader/ViewModels/ViewModelBase.cs
@@ -3,6 +3,7 @@ using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
 using System;
 using System.Collections.Generic;
+using System.Reactive.Linq;
 using System.Text;
 
 namespace CurseForgeDownloader.ViewModels
@@ -11,24 +12,28 @@ namespace CurseForgeDownloader.ViewModels
     {
         protected IObservable<bool> IsBusyObservable { get; init; }
 
-        private string? _statusText;
-        public string? StatusText
-        {
-            get => _statusText;
-            set => Dispatcher.UIThread.Post(() => this.RaiseAndSetIfChanged(ref _statusText, value));
-        }
-        private bool _isBusy;
-        public bool IsBusy
-        {
-            get => _isBusy;
-            set => Dispatcher.UIThread.Post(() => this.RaiseAndSetIfChanged(ref _isBusy, value));
-        }
+        // private string? _statusText;
+        // public string? StatusText
+        // {
+        //     get => _statusText;
+        //     set => Dispatcher.UIThread.Post(() => this.RaiseAndSetIfChanged(ref _statusText, value));
+        // }
+        // private bool _isBusy;
+        // public bool IsBusy
+        // {
+        //     get => _isBusy;
+        //     set => Dispatcher.UIThread.Post(() => this.RaiseAndSetIfChanged(ref _isBusy, value));
+        // }
+        [Reactive]
+        public string? StatusText { get; set; }
+        [Reactive]
+        public bool IsBusy { get; set; }
         public bool HasError { get; set; }
 
 
         protected ViewModelBase()
         {
-            IsBusyObservable = this.WhenAnyValue(vm => vm.IsBusy, isBusy => !isBusy);
+            IsBusyObservable = this.WhenAnyValue(vm => vm.IsBusy, isBusy => !isBusy).ObserveOn(RxApp.MainThreadScheduler);
             IsBusyObservable.Subscribe((isBusy) =>
             {
                 if (isBusy)
